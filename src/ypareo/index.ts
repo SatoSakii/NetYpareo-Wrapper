72b4cdbde7feb2351fdf1ff716758fc35c42b841
SatoSakii <satohaki.ber@gmail.com>
2025-12-03 12:12:20 +0100

    fix(export): add barrel exports + default exports for attendance

--- src/ypareo/index.ts
+++ src/ypareo/index.ts
@@ -4,3 +4,4 @@ export * from "./core";
 export * from "./types";
 export * from "./constants";
 export * from "./parsers"
+export * from "./managers";
--- src/ypareo/models/index.ts
+++ src/ypareo/models/index.ts
@@ -1,2 +1,3 @@
 export { User } from "./User";
 export * from "./planning";
+export * from "./attendance";
